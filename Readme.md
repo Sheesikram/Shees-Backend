cf1290c9a7213403f059c1ab669742b189a5d9c6
sheesikram <sheesikram20@gmail.com>
2024-12-05 17:42:05 +0500

    connent to MongoDB atlas Established

--- Readme.md
+++ Readme.md
@@ -7,3 +7,7 @@ nodemon is a tool that helps develop Node.js based applications by automatically
     "dev": "nodemon src/index.js"
   },
 Prettier is an opinionated code formatter. It enforces a consistent style by parsing your code and re-printing it with its own rules that take the maximum line length into account, wrapping code when necessary.-dev depandancy
+for database connection we used mongodb atlass and make a network access and 0.0.0.0/0
+(includes your current IP address)
+and databace access also --
+when ever you select database always use try catch or async wait(db is always iin other continenet)
---
+++ src/index.js
@@ -0,0 +1,33 @@
+//require('dotenv').config({path:'./env'});
+import dotenv from "dotenv";
+import mongoose from "mongoose";    
+import express from "express";
+import {DB_NAME} from "./constants.js"
+import connectDB from "./db/index.js";
+
+dotenv.config(
+    {path:'./env'}
+);
+
+connectDB();
+
+//Approach 2 for db connection is in db folder index.js
+//Aproach 1 for db connection
+// const app = express();
+
+// (async () => {  //async function used for db on other continent :)
+//   try {
+//    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//    app.on("error",(error)=>{
+//     console.log("Error connecting to DB",error);
+//     throw error;
+// });
+//     app.listen(process.env.PORT, () => {
+//       console.log( `Server is running on port ${process.env.PORT}`);
+//     });
+
+//   } catch (error) {
+//     console.log("Error connecting to DB", error);
+//     throw error;
+//   }
+// })(); 
